42a7f946124f279b1e7059bf7c3d3f4cbac5c3ed
Nguyen Cong Duy <ncongduy@gmail.com>
2022-02-08 16:01:20 +0200

    step 1: create custom hook useCountries

--- src/custom-hooks/useCountries.js
+++ src/custom-hooks/useCountries.js
@@ -1,1 +1,19 @@
 // This hook is used to fetch all countries
+import { useEffect, useState } from 'react';
+
+import fetchData from './fechApi';
+
+function useCountries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchData.getAllCountries();
+      setCountries(data);
+    })();
+  }, []);
+
+  return countries;
+}
+
+export default useCountries;
