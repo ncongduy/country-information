01f344f1031e47afa110daf94c6f455ee73be4e3
Nguyen Cong Duy <ncongduy@gmail.com>
2022-02-08 16:02:52 +0200

    step 1: create custom hook useCountry

--- src/custom-hooks/useCountry.js
+++ src/custom-hooks/useCountry.js
@@ -1,1 +1,19 @@
 // This hook is used to fetch one specific country
+import { useEffect, useState } from 'react';
+
+import fetchData from './fechApi';
+
+function useCountry(nameCountry) {
+  const [country, setCountry] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchData.searchCountry(nameCountry);
+      setCountry(data);
+    })();
+  }, [nameCountry]);
+
+  return country;
+}
+
+export default useCountry;
