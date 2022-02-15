585e2fc994684b02301b0e83c6d212bdf5107611
Nguyen Cong Duy <ncongduy@gmail.com>
2022-02-15 19:21:19 +0200

    step 7: create ThemeContext by using Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
+import ThemeContext from './contexts/ThemeContext';
+import HeaderApp from './components/HeaderApp';
 import HomePage from './pages/Home';
 import CountryPage from './pages/Country';
 import './App.css';
@@ -7,10 +9,13 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/:countryName" element={<CountryPage />} />
-      </Routes>
+      <ThemeContext>
+        <HeaderApp />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:countryName" element={<CountryPage />} />
+        </Routes>
+      </ThemeContext>
     </div>
   );
 }
---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, useMemo, useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+export const ColorModeContext = createContext({ toggleColorMode: () => {} });
+
+export default function ThemeContext({ children }: any) {
+  const [mode, setMode] = useState<'light' | 'dark'>('light');
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
