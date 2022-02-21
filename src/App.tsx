49155f60cef86e73d4a57f03c26fcde21bb3e8d1
Nguyen Cong Duy <ncongduy@gmail.com>
2022-02-21 10:59:57 +0700

    refactor: App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 
-import ThemeContext from './contexts/ThemeContext';
-import HeaderApp from './components/HeaderApp';
+import AppContext from './contexts';
+import AppHeader from './components/HeaderApp';
 import HomePage from './pages/Home';
 import CountryPage from './pages/Country';
-import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <ThemeContext>
-        <HeaderApp />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/:countryName" element={<CountryPage />} />
-        </Routes>
-      </ThemeContext>
-    </div>
+    <AppContext>
+      <AppHeader />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryName" element={<CountryPage />} />
+      </Routes>
+    </AppContext>
   );
 }
 
